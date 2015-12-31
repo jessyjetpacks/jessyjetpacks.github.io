fd33e565e5afb2f65a7b993abbb9fbb2824836bf
Nathan Koren <nkoren@gmail.com>
2015-12-31 14:58:06 +0000

    Light-boxing is working

--- js/jetpacks.js
+++ js/jetpacks.js
@@ -1,14 +1,16 @@
 $( document ).ready( function () {
-	var gal;
-	_.each( jetpacksGallery, function( gallery, key ) {
+	var gal, title, description;
+	_.each( jetpacksGallery, function(gallery, key) {
 		$( "#portfolio-nav" ).append( "<div class='col-sm-4 col-xs-6'><a href='#" + key + "'>" + gallery.title + "</a></div>" );
 		gal = $( "<div id='" + key + "' class='row portfolio portfolio-" + key + "'><div class='col-md-12'>"
 			+ "<div class='row'><div class='col-md-12'><a name='" + key + "'></a><h3>" + gallery.title + "</h3></div></div>"
 			+ "</div></div>" ).appendTo( "#portfolio-gallery" );
 
-		_.each( gallery.pictures, function( picture ) {
+		_.each( gallery.pictures, function(picture) {
+			title = picture.title ? "<span class=\"picture_title\">" + picture.title.replace(/\'/gim, "&#39") + "</span>" : "";
+			description = picture.description ? '<span class=\"picture_description\">' + picture.description.replace(/\'/gim, "&#39") + '</span>': "";
 			gal.append( "<div class='image col-sm-2 col-xs-6'>"
-				+ "<a href='img/images/" + picture.source + "'>"
+				+ "<a href='img/images/" + picture.source + "' data-lightbox='" + key + "' data-title='" + title + description + "'>"
 				+ "<img src='img/thumbnails/" + picture.source + "'>"
 				+ "</a></div>" );
 		})
