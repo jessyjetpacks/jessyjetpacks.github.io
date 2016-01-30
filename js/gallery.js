f73e4918fbeb9164880b8d1061980847e1224a60
jessyjetpacks <jessy_richards@hotmail.co.uk>
2016-01-30 22:09:47 +0000

    add images changed category order and names

--- js/gallery.js
+++ js/gallery.js
@@ -15,8 +15,110 @@ jetpacksGallery = {
 		]
 	},
 */
-	"clothing" : {
-		title: "Clothing",
+	"painting" : {
+		title: "Paint / Collage / Print",
+		pictures: [
+				{
+				source: "p10.jpg",
+				title: "Detail of 'With you gone there is still always cats",
+				year: 2014,
+				month: 1
+			},	{
+				source: "p11.jpg",
+				title: "Love triangles",
+				year: 2014,
+				month: 1
+			},	{
+				source: "p11a.jpg",
+				title: "Love triangle",
+				description: "Bottom pyramid",
+				year: 2014,
+				month: 1
+			},	{
+				source: "p11b.jpg",
+				title: "Love triangle",
+				description: "Top pyramid",
+				year: 2014,
+				month: 1
+			},	{
+				source: "p12.jpg",
+				title: "In perfect love and perfect trust",
+				year: 2014,
+				month: 1
+			},	{
+				source: "p13.jpg",
+				title: "Baboon culture",
+				year: 2014,
+				month: 1
+			},	{
+				source: "p13a.jpg",
+				title: "Neutral aim",
+				year: 2014,
+				month: 1
+			},	{
+				source: "p13b.jpg",
+				title: "Baboon culture",
+				year: 2014,
+				month: 1
+			},	{
+				source: "p13c.jpg",
+				title: "Baboon culture",
+				year: 2014,
+				month: 1
+			}
+		]
+	},
+
+	"sculpture" : {
+		title: "Sculpture / Installation",
+		pictures: [
+			{
+				source: "ggg.jpg",
+				title: "3D test prints",
+				year: 2015,
+				month: 11
+			},
+			{
+				source: "DSC04267.jpg",
+				title: "I love your fruiting body",
+				year: 2014,
+				month: 1
+			},
+			{
+				source: "DSC04269.jpg",
+				title: "I love your fruiting body",
+				year: 2014,
+				month: 1
+			},
+			{
+				source: "DSC04274.jpg",
+				title: "I love your fruiting body",
+				year: 2014,
+				month: 1
+			},
+			{
+				source: "DSC04266.jpg",
+				title: "I love your fruiting body",
+				year: 2014,
+				month: 1
+			},
+		]
+	},
+
+	"drawing" : {
+		title: "Drawing / Sketchbook",
+		pictures: [
+			{
+				source: "c31.jpg",
+				title: "Baboon culture",
+				year: 2014,
+				month: 1
+			},
+		]
+	},
+
+	"Textile" : {
+		title: "Textile",
 		pictures: [
 			{
 				source: "c19.jpg",
@@ -104,226 +206,10 @@ jetpacksGallery = {
 
 	},
 
-	"painting" : {
-		title: "Painting",
-		pictures: [
-
-				{
-				source: "p10.jpg",
-				title: "Detail of 'With you gone there is still always cats",
-				year: 2014,
-				month: 1
-			},	{
-				source: "p11.jpg",
-				title: "Love triangles",
-				year: 2014,
-				month: 1
-			},	{
-				source: "p11a.jpg",
-				title: "Love triangle",
-				description: "Bottom pyramid",
-				year: 2014,
-				month: 1
-			},	{
-				source: "p11b.jpg",
-				title: "Love triangle",
-				description: "Top pyramid",
-				year: 2014,
-				month: 1
-			},	{
-				source: "p12.jpg",
-				title: "In perfect love and perfect trust",
-				year: 2014,
-				month: 1
-			},	{
-				source: "p13.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "p13a.jpg",
-				title: "Neutral aim",
-				year: 2014,
-				month: 1
-			},	{
-				source: "p13b.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "p13c.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},	{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},
-
-		]
-	},
-
-	"drawing" : {
-		title: "Drawing / Sketchbooks",
-		pictures: [
-			{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},
-		]
-	},
-
-	"sculpture" : {
-		title: "Sculpture / Installation",
-		pictures: [
-			{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},
-		]
-	},
-
-	"print" : {
-		title: "Print / Collage",
-		pictures: [
-			{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},
-		]
-	},
-
-	"photographs" : {
-		title: "Photographs",
+	"Stills" : {
+		title: "Stills",
 		pictures: [
 		]
 	},
-
-	"performance" : {
-		title: "Performance / Happenings",
-		pictures: [
-			{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},
-		]
-	},
-
-	"textile" : {
-		title: "Textile",
-		pictures: [
-			{
-				source: "c31.jpg",
-				title: "Baboon culture",
-				year: 2014,
-				month: 1
-			},
-		]
-	}
 
 }
